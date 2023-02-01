11b6e0fb5c8d9e766a5d76ecbf85ca62df8e8b0a
Mubosher Muydinov <muboshermuydinov5@gmail.com>
2023-02-01 09:31:48 +0300

    build

--- src/app/user/components/single-surah-main/single-surah-main.component.ts
+++ src/app/user/components/single-surah-main/single-surah-main.component.ts
@@ -1,24 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import { Store } from '@ngrx/store';
 import { userStataType } from '../../store/reducer';
+import { VerseBySurahs } from '../../types';
 @Component({
   templateUrl: './single-surah-main.component.html',
   styleUrls: ['./single-surah-main.component.scss'],
   selector: 'app-single-surah-main',
 })
-export class SingleSurahMainComponent {
+export class SingleSurahMainComponent implements OnInit {
   isTranslation: boolean = false;
   faPlayCircle = faPlayCircle;
+  pageNumber: number = 1;
   verseIndex: number = 0;
   arabicVerse: string = '';
   userState!: userStataType;
+  verseClone!: VerseBySurahs[];
+  pageAbleVerse!: VerseBySurahs[];
   constructor(private store: Store<{ user: userStataType }>) {
     this.store.select('user').subscribe((result) => {
       this.userState = result;
     });
   }
-
+  ngOnInit(): void {}
   verseControl(verseType: string) {
     if (this.userState.verse) {
       if (this.verseIndex < this.userState.verse?.length - 1) {
